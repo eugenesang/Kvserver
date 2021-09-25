2ceb4e58ba010065982c3001a1a54fe11701e025
Eugene Kiprop Sang <85967099+eugenesang@users.noreply.github.com>
2021-09-25 11:05:35 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const mongoose=require("mongoose");
+const express=require("express");
+const uri="mongodb+srv://eugenesang:24Kmagic@cluster0.wa6ab.mongodb.net/houses?retryWrites=true&w=majority";
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+mongoose.connect(uri,{useNewUrlParser:true, useUnifiedTopology:true})
+.then(data=>{
+    app.listen(3001, ()=>{console.log("app listening on port 3001")});
+})
+.catch(err=>console.trace(err));
+app.use("/news", require("./routes/news"));
+app.use("/posts", require("./routes/posts"));
+app.use("/houses", require("./routes/houses"));
+app.use("/goods", require("./routes/goods"));
+app.use('/login', require("./routes/login"));
+app.use("/register", require("./routes/register"))
